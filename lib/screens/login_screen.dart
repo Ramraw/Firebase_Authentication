79ad423c5ac9c3a0b93fe50942f1cd50d9651ec1
seema yadav <ramvijayya2004@gmail.com>
2023-07-29 13:33:34 +0530

    commited

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_authentication/screens/login_email_password_screen.dart';
+import 'package:firebase_authentication/screens/phone_screen.dart';
+import 'package:firebase_authentication/screens/signup_email_password_screen.dart';
+import 'package:firebase_authentication/services/firebase_auth_methods.dart';
+import 'package:firebase_authentication/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            CustomButton(
+              onTap: () {
+                Navigator.pushNamed(context, EmailPasswordSignup.routeName);
+              },
+              text: 'Email/Password Sign Up',
+            ),
+            CustomButton(
+              onTap: () {
+                Navigator.pushNamed(context, EmailPasswordLogin.routeName);
+              },
+              text: 'Email/Password Login',
+            ),
+            CustomButton(
+                onTap: () {
+                  Navigator.pushNamed(context, PhoneScreen.routeName);
+                },
+                text: 'Phone Sign In'),
+            CustomButton(
+              onTap: () {
+                context.read<FirebaseAuthMethods>().signInWithGoogle(context);
+              },
+              text: 'Google Sign In',
+            ),
+            CustomButton(
+              onTap: () {
+                context.read<FirebaseAuthMethods>().signInWithFacebook(context);
+              },
+              text: 'Facebook Sign In',
+            ),
+            CustomButton(
+              onTap: () {
+                context.read<FirebaseAuthMethods>().signInAnonymously(context);
+              },
+              text: 'Anonymous Sign In',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
